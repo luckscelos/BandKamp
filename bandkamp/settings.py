c09dbfb3415cd489f11ba0abbd8b94c00022a24f
luckscelos <lucasxmb800@gmail.com>
2022-12-30 11:16:16 -0300

    deploy

--- bandkamp/settings.py
+++ bandkamp/settings.py
@@ -33,7 +33,7 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv("DEBUG", False)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["web-production-7fde.up.railway.app", "0.0.0.0"]
 
 
 # Application definition
